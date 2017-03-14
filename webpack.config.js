c901d8925ceaab166a0775b1d45f4733f08fbbc8
pratik <lazy.monk.13@gmail.com>
2017-03-14 09:12:01 -0700

    webpack dev server running

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack');
+
+module.exports = {
+	entry: [
+		'./src/index.js'
+	],
+	output: {
+		path: __dirname + '/dist',
+		publicPath: '/',
+		filename: 'bundle.js'
+	},
+	devServer: {
+		contentBase: './dist'
+	}
+}
