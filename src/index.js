de28640f12d9f696cf0bf078ada636a7b1aa400e
pratik <lazy.monk.13@gmail.com>
2017-03-14 09:35:16 -0700

    Rendering from React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hello')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+	<div>
+		<h1>hello from react</h1>
+	</div>
+)
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('react')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,30 @@
+const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+	devServer: {
+		contentBase: './dist',
+		hot: true
+	},
 	entry: [
+		'webpack-dev-server/client?http://localhost:8080',
+		'webpack/hot/only-dev-server',
 		'./src/index.js'
 	],
+	module: {
+		loaders: [
+			{test: /\.jsx?$/, exclude: /node_modules/, loader: 'react-hot-loader!babel-loader'}
+		]
+	},
 	output: {
 		path: __dirname + '/dist',
 		publicPath: '/',
 		filename: 'bundle.js'
 	},
-	devServer: {
-		contentBase: './dist'
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	],
+	resolve: {
+		extensions: ['.js', '.jsx']
 	}
 }
